e5ec3db323b8c34d88a643f6ab73464e9755645a
César Soto Valero <cesarsotovalero@gmail.com>
2022-12-18 22:01:02 +0100

    Capture tests results from logs

--- pipeline/src/main/java/se/kth/castor/deptrim/experiments/App.java
+++ pipeline/src/main/java/se/kth/castor/deptrim/experiments/App.java
@@ -18,11 +18,15 @@ public class App {
         File splDeps = new File("csv/specialized-deps.csv");
         File origDeps = new File("csv/original-deps.csv");
         File deptrimLogs = new File("csv/deptrim-logs.csv");
+        File originalBuildLog = new File("csv/original-build-log.csv");
+        File specializedBuildLog = new File("csv/specialized-build-log.csv");
 
         // Write file headers
         FileUtils.writeStringToFile(splDeps, "Project,Dependency,FilePath,Size" + "\n", true);
         FileUtils.writeStringToFile(origDeps, "Project,Dependency,FilePath,Size" + "\n", true);
         FileUtils.writeStringToFile(deptrimLogs, "Project,FilePath,Dependency,NA,RemovedTypes,TotalTypes" + "\n", true);
+        FileUtils.writeStringToFile(originalBuildLog, "Project,FilePath,TestRun,Failures,Errors,Skipped" + "\n", true);
+        FileUtils.writeStringToFile(specializedBuildLog, "Project,FilePath,TestRun,Failures,Errors,Skipped" + "\n", true);
 
         try (Stream<Path> filepath = Files.walk(Paths.get("results"))) {
             // process the /deptrim/ directory
@@ -33,10 +37,60 @@ public class App {
                     processCompileScopeJars(origDeps, f);
                 } else if (f.toString().endsWith("deptrim/deptrim.log")) {
                     processDeptrimLogs(deptrimLogs, f);
+                } else if (f.toString().endsWith("original/maven.log")) {
+                    processTestsInBuildLog(originalBuildLog, f);
+                } else if (f.toString().endsWith("pom-specialized/maven.log")) {
+                    processTestsInBuildLog(specializedBuildLog, f);
                 }
             });
         } catch (IOException e) {
             throw new IOException("Directory Not Present!");
+        }
+    }
+
+    private static void processTestsInBuildLog(File buildLog, Path f) {
+        List<String> l = new ArrayList<>();
+        try {
+            List<String> lines = Files.readAllLines(f);
+            for (String line : lines) {
+                if (line.startsWith("Tests run: ")) {
+                    String project = f.toString().split("/")[1];
+                    String testRun = line.split(" ")[2].substring(0, line.split(" ")[2].length() - 1);
+                    String failures = line.split(" ")[4].substring(0, line.split(" ")[4].length() - 1);
+                    String errors = line.split(" ")[6].substring(0, line.split(" ")[6].length() - 1);
+                    String skipped = line.split(" ")[8].substring(0, line.split(" ")[8].length());
+                    l.add(project + "," + f + "," +
+                            testRun + "," +
+                            failures + "," +
+                            errors + "," +
+                            skipped + "\n"
+                    );
+                } else if (line.startsWith("[INFO] Tests run: ") || line.startsWith("[WARNING] Tests run: ")) {
+                    String project = f.toString().split("/")[1];
+                    String testRun = line.split(" ")[3].substring(0, line.split(" ")[3].length() - 1);
+                    String failures = line.split(" ")[5].substring(0, line.split(" ")[5].length() - 1);
+                    String errors = line.split(" ")[7].substring(0, line.split(" ")[7].length() - 1);
+                    String skipped = line.split(" ")[9].substring(0, line.split(" ")[9].length());
+                    l.add(project + "," + f + "," +
+                            testRun + "," +
+                            failures + "," +
+                            errors + "," +
+                            skipped + "\n"
+                    );
+                }
+            }
+            // Write the last test run line
+            if (!l.isEmpty()) {
+                FileUtils.writeStringToFile(buildLog, l.get(l.size() - 1), true);
+            } else {
+                FileUtils.writeStringToFile(buildLog, f.toString().split("/")[1] + "," + f + "," +
+                        "NA" + "," +
+                        "NA" + "," +
+                        "NA" + "," +
+                        "NA" + "\n", true);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
@@ -91,6 +145,5 @@ public class App {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 }
